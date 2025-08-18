1667ee16aa1b212a43d3faaa35238649325f2cc4
mblees <mariusblees@gmail.com>
2025-08-18 13:22:22 +0200

    added the ability to swap between gamemodes

--- Ballongame.py
+++ Ballongame.py
@@ -1,15 +1,35 @@
-import src.gamemodes
 import logging
+import time
+
+from logging_config import activate_logging_config
+
+from src.gamemodes import Modes, GenericGamemode, EasyMode, MediumMode, HardMode
 
 
 class Ballongame:
     def __init__(self):
         self.logger = logging.getLogger("Ballongame")
+        self.logger.setLevel(logging.DEBUG)
+        self.mode: GenericGamemode = EasyMode()
+        self._running = True
 
     def run(self):
-        pass
+        self.logger.info("Starting Game Loop!")
+        while self._running:
+            self.mode.print_mode()
+            self.change_mode()
+            time.sleep(2)
+
+    def change_mode(self):
+        if isinstance(self.mode, EasyMode):
+            self.mode = MediumMode()
+        elif isinstance(self.mode, MediumMode):
+            self.mode = HardMode()
+        elif isinstance(self.mode, HardMode):
+            self.mode = EasyMode()
 
 
 if __name__ == "__main__":
+    activate_logging_config()
     ballongame = Ballongame()
     ballongame.run()
--- src/gamemodes.py
+++ src/gamemodes.py
@@ -1,12 +1,17 @@
 import logging
+from enum import Enum
 
 
 class GenericGamemode:
     def __init__(self, logging_name: str):
         self.logger = logging.getLogger(logging_name)
+        self.mode: str = logging_name
 
     def run_gameloop(self):
         self.logger.warning("Using generic Gamemode Class. Overwrite this function.")
+
+    def print_mode(self):
+        self.logger.info(f"Mode is set to: {str(self.mode)}")
 
 
 class EasyMode(GenericGamemode):
@@ -24,9 +29,17 @@ class MediumMode(GenericGamemode):
     def run_gameloop(self):
         pass
 
+
 class HardMode(GenericGamemode):
     def __init__(self):
         super().__init__("Hard Mode")
 
     def run_gameloop(self):
         pass
+
+
+class Modes(Enum):
+    EASY = lambda: EasyMode()
+    MEDIUM = lambda: MediumMode()
+    HARD = lambda: HardMode()
+
