a770d5c3bc77747330368f1fb5986805ec25d925
mblees <mariusblees@gmail.com>
2025-08-18 13:03:15 +0200

    added function placeholders

--- src/hardware.py
+++ src/hardware.py
@@ -1,6 +1,48 @@
 import logging
 
 
-class Hardware:
+class Pump:
     def __init__(self):
-        self.logger = logging.getLogger("Hardware")
+        self.logger = logging.getLogger("Pump")
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class ReleaseValve:
+    def __init__(self):
+        self.logger = logging.getLogger("ReleaseValve")
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class LED:
+    def __init__(self):
+        self.logger = logging.getLogger("LED")
+
+    def set_color(self, color: tuple[int, int, int]):
+        pass
+
+    def turn_off(self):
+        pass
+
+    def turn_on(self):
+        pass
+
+
+class Speaker:
+    def __init__(self):
+        self.logger = logging.getLogger("Speaker")
+
+    def play_sound(self, sound_id: str):
+        pass
+
+    def stop_sound(self):
+        pass
