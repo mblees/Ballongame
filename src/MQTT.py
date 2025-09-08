aeb4a548ddb1d1ee4b275f6700b24efa4ebda758
mblees <mariusblees@gmail.com>
2025-09-08 18:45:53 +0200

    test

--- src/MQTT.py
+++ src/MQTT.py
@@ -1,12 +1,12 @@
 import paho.mqtt.client as mqtt
 
+
 class MQTTClient:
     def __init__(self, broker, port=1883, username=None, password=None):
         self.client = mqtt.Client()
         self.broker = broker
         self.port = port
         self.callback = None
-        self.topics = []
 
         if username and password:
             self.client.username_pw_set(username, password)
@@ -18,8 +18,6 @@ class MQTTClient:
 
     def _on_connect(self, client, userdata, flags, rc):
         print("Verbunden mit Code", rc)
-        for topic in self.topics:
-            client.subscribe(topic)
 
     def _on_message(self, client, userdata, msg):
         if self.callback:
@@ -29,8 +27,7 @@ class MQTTClient:
         self.callback = callback
 
     def subscribe(self, topic):
-        self.topics.append(topic)
-        self.client.subscribe(topic)  # direkt abonnieren, falls schon verbunden
+        self.client.subscribe(topic)
 
     def start(self):
         self.client.connect(self.broker, self.port, 60)
