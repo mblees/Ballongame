28e77824a0a7d43cf1f21a0fa1db69377fe4a555
mblees <mariusblees@gmail.com>
2025-09-10 19:36:21 +0200

    fixed interrupt implementation

--- src/Hardware.py
+++ src/Hardware.py
@@ -79,4 +79,5 @@ class Button:
         return self.pi.read(self.io) == 1
 
     def enable_interrupt(self, callback):
-        self.pi.add_interrupt_callback(self.io, callback)
+        self.pi.callback(self.io, pigpio.EITHER_EDGE, callback)
+
