49be8566d21fd5f29ac892ede043281a2ddf6bbc
Marius Blees <mariusblees@gmail.com>
2025-09-11 13:22:42 +0200

    changed angles

--- src/Hardware.py
+++ src/Hardware.py
@@ -173,7 +173,7 @@ class MiuzeiDigitalServo:  # 20kg Servo
         self.max_pulse = max_pulse
         self.current_angle = None
 
-        self.eject_angle = 90
+        self.eject_angle = 0
         self.normal_angle = 130
 
         # Set GPIO as output (pigpio handles PWM on it)
